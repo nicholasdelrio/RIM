975ee00d3168c325999dc55a4eb72fec2d3017bc
Nick <ndel2@utep.edu>
2014-07-23 07:10:31 -0400

    adding converion triggers and e params

---
+++ data/source/deana-pennington/rim-focus/version/2013-06/convert-rim-focus.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+#
+#3 <#> a <http://purl.org/twc/vocab/conversion/ConversionTrigger> ;
+#3     rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger>,
+#3                  <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-create-convert-sh.sh> .
+#
+# datasetID versionID (lastModDate):
+# rim-focus 2013-06 ()
+#--------------------------------------------------------------
+
+see="https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source my-csv2rdf4lod-source-me.sh or see \$see"}
+
+# The identifiers used to name the dataset that will be converted.
+#            (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phase:-name)
+surrogate="http://visko.cybershare.utep.edu/linked-data" # Came from $CSV2RDF4LOD_BASE_URI when cr-create-convert-sh.sh created this script.
+sourceID="deana-pennington"               # Came from directory name ../../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)
+datasetID="rim-focus"             # Came from directory name ../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)
+datasetVersion="2013-06"        # DEPRECATED
+versionID="2013-06"             # Came from directory name ../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions) renaming datasetVersion (deprecating datasetVersion)
+eID="1"                             # enhancement identifier
+if [[ "$1" == "-e" && $# -ge 2 ]]; then
+   eID="$2" # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Generating-enhancement-parameters
+   shift 2
+fi
+
+cr_justdoit="no"
+if [[ "$1" == "--force" ]]; then
+   cr_justdoit="yes" # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger#--force
+   shift
+fi
+
+if [ -d doc/logs ]; then
+   dateInXSDDateTime.sh > doc/logs/conversion-trigger-last-pulled
+fi
+
+destDir="automatic"                 # convention has led to always be 'automatic'; the directory where the converted RDF is put.
+#--------------------------------------------------------------
+
+
+#-----------------------------------
+# source/deana-projects.csv
+sourceDir="source"
+datafile="deana-projects.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="deana-projects"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# source/people.csv
+sourceDir="source"
+datafile="people.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="people"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# source/project-relationships.csv
+sourceDir="source"
+datafile="project-relationships.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="project-relationships"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# source/water-sustainability-projects.csv
+sourceDir="source"
+datafile="water-sustainability-projects.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="water-sustainability-projects"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+source $CSV2RDF4LOD_HOME/bin/convert-aggregate.sh
+# ^^ Note, this step can also be done manually using publish/bin/publish.sh
